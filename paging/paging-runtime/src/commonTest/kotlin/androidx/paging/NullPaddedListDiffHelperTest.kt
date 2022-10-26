ddedae77809683a94e05ceee52b635733eb26376
Veyndan Stuart <veyndan@gmail.com>
2022-10-26 14:34:42 +0200

    [testutils-paging] Move jvmMain to commonMain

--- paging/paging-runtime/src/commonTest/kotlin/androidx/paging/NullPaddedListDiffHelperTest.kt
+++ paging/paging-runtime/src/commonTest/kotlin/androidx/paging/NullPaddedListDiffHelperTest.kt
@@ -24,13 +24,10 @@ import androidx.paging.ListUpdateCallbackFake.OnInsertedEvent
 import androidx.paging.ListUpdateCallbackFake.OnMovedEvent
 import androidx.paging.ListUpdateCallbackFake.OnRemovedEvent
 import androidx.recyclerview.widget.DiffUtil
-import com.google.common.truth.Truth.assertThat
-import org.junit.Assert.assertEquals
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
 
-@RunWith(JUnit4::class)
 class NullPaddedListDiffHelperTest {
     class Storage(
         override val placeholdersBefore: Int,
@@ -140,19 +137,20 @@ class NullPaddedListDiffHelperTest {
             Storage(5, listOf("a", "b", "c"), 5),
             Storage(5, listOf("c", "x", "y", "a", "b"), 5)
         ) {
-            assertThat(
-                it.allEvents
-            ).containsExactly(
-                // convert 2 placeholders to x,y
-                OnChangedEvent(3, 2, PLACEHOLDER_TO_ITEM),
-                // move c to before x,y
-                OnMovedEvent(7, 3),
-                // these placeholders will shift down in the list as we'll re-add 2 placeholders
-                OnChangedEvent(0, 3, PLACEHOLDER_POSITION_CHANGE),
-                // now we need 2 new placeholders
-                OnInsertedEvent(0, 2),
-                // all trailing placeholders shifted 2 positions
-                OnChangedEvent(10, 5, PLACEHOLDER_POSITION_CHANGE),
+            assertContentEquals(
+                listOf(
+                    // convert 2 placeholders to x,y
+                    OnChangedEvent(3, 2, PLACEHOLDER_TO_ITEM),
+                    // move c to before x,y
+                    OnMovedEvent(7, 3),
+                    // these placeholders will shift down in the list as we'll re-add 2 placeholders
+                    OnChangedEvent(0, 3, PLACEHOLDER_POSITION_CHANGE),
+                    // now we need 2 new placeholders
+                    OnInsertedEvent(0, 2),
+                    // all trailing placeholders shifted 2 positions
+                    OnChangedEvent(10, 5, PLACEHOLDER_POSITION_CHANGE),
+                ),
+                it.allEvents,
             )
         }
     }
@@ -163,11 +161,12 @@ class NullPaddedListDiffHelperTest {
             Storage(5, listOf("a", "b", "c"), 5),
             Storage(3, listOf("c", "x", "y", "a", "b"), 5)
         ) {
-            assertThat(
-                it.allEvents
-            ).containsExactly(
-                OnChangedEvent(3, 2, PLACEHOLDER_TO_ITEM),
-                OnMovedEvent(7, 3)
+            assertContentEquals(
+                listOf(
+                    OnChangedEvent(3, 2, PLACEHOLDER_TO_ITEM),
+                    OnMovedEvent(7, 3),
+                ),
+                it.allEvents,
             )
         }
     }
@@ -178,15 +177,16 @@ class NullPaddedListDiffHelperTest {
             Storage(5, listOf("a", "b", "c"), 5),
             Storage(5, listOf("b", "c", "x", "y", "a"), 5)
         ) {
-            assertThat(
-                it.allEvents
-            ).containsExactly(
-                // insert x, y as placeholder changes
-                OnChangedEvent(8, 2, PLACEHOLDER_TO_ITEM),
-                // move a to after x,y
-                OnMovedEvent(5, 9),
-                // insert new placeholders to the end
-                OnInsertedEvent(13, 2)
+            assertContentEquals(
+                listOf(
+                    // insert x, y as placeholder changes
+                    OnChangedEvent(8, 2, PLACEHOLDER_TO_ITEM),
+                    // move a to after x,y
+                    OnMovedEvent(5, 9),
+                    // insert new placeholders to the end
+                    OnInsertedEvent(13, 2)
+                ),
+                it.allEvents,
             )
         }
     }
@@ -197,21 +197,22 @@ class NullPaddedListDiffHelperTest {
             Storage(4, listOf("a", "b", "c", "d", "e"), 1),
             Storage(1, listOf("d", "e", "f", "g"), 20)
         ) {
-            assertThat(
-                it.allEvents
-            ).containsExactly(
-                // add f to replace the placeholder after e
-                OnChangedEvent(9, 1, PLACEHOLDER_TO_ITEM),
-                // add g, we don't have a placeholder for it so it is an insertion
-                OnInsertedEvent(10, 1),
-                // rm a,b,c
-                OnRemovedEvent(4, 3),
-                // rm 3 unnecessary leading placeholders
-                OnRemovedEvent(0, 3),
-                // 3rd placeholder moved to pos 0, so dispatch a change for it
-                OnChangedEvent(0, 1, PLACEHOLDER_POSITION_CHANGE),
-                // add 20 trailing placeholders
-                OnInsertedEvent(5, 20)
+            assertContentEquals(
+                listOf(
+                    // add f to replace the placeholder after e
+                    OnChangedEvent(9, 1, PLACEHOLDER_TO_ITEM),
+                    // add g, we don't have a placeholder for it so it is an insertion
+                    OnInsertedEvent(10, 1),
+                    // rm a,b,c
+                    OnRemovedEvent(4, 3),
+                    // rm 3 unnecessary leading placeholders
+                    OnRemovedEvent(0, 3),
+                    // 3rd placeholder moved to pos 0, so dispatch a change for it
+                    OnChangedEvent(0, 1, PLACEHOLDER_POSITION_CHANGE),
+                    // add 20 trailing placeholders
+                    OnInsertedEvent(5, 20)
+                ),
+                it.allEvents,
             )
         }
     }
@@ -327,14 +328,17 @@ class NullPaddedListDiffHelperTest {
             Storage(4, listOf("c_4", "4", "d_5", "5", "e_6", "6"), 3),
             Storage(0, listOf("a_0", "0", "b_1", "1"), 8)
         ) {
-            assertThat(it.allEvents).containsExactly(
-                // replace previous items with placeholders
-                OnChangedEvent(4, 6, ITEM_TO_PLACEHOLDER),
-                // swap first 4 placeholders with newly loaded items
-                OnChangedEvent(0, 4, PLACEHOLDER_TO_ITEM),
-                // remove extra placeholder
-                OnRemovedEvent(12, 1)
-            ).inOrder()
+            assertContentEquals(
+                listOf(
+                    // replace previous items with placeholders
+                    OnChangedEvent(4, 6, ITEM_TO_PLACEHOLDER),
+                    // swap first 4 placeholders with newly loaded items
+                    OnChangedEvent(0, 4, PLACEHOLDER_TO_ITEM),
+                    // remove extra placeholder
+                    OnRemovedEvent(12, 1)
+                ),
+                it.allEvents,
+            )
         }
     }
 
@@ -344,17 +348,20 @@ class NullPaddedListDiffHelperTest {
             Storage(4, listOf("a_4", "4", "b_5", "5", "c_6", "6"), 3),
             Storage(8, listOf("d_8", "8", "e_9", "9"), 0),
         ) {
-            assertThat(it.allEvents).containsExactly(
-                // remove c_6 and 6, their positions overlap w/ newly loaded items
-                OnRemovedEvent(8, 2),
-                // now insert d_8 and 8
-                OnInsertedEvent(8, 2),
-                // first 4 of the loaded items becomes placeholders: "a_4", "4", "b_5", "5"
-                OnChangedEvent(4, 4, ITEM_TO_PLACEHOLDER),
-                // insert e_9 and 9 using placeholders
-                OnChangedEvent(10, 2, PLACEHOLDER_TO_ITEM),
-                // finally, remove the last placeholder that we won't use
-                OnRemovedEvent(12, 1)
+            assertContentEquals(
+                listOf(
+                    // remove c_6 and 6, their positions overlap w/ newly loaded items
+                    OnRemovedEvent(8, 2),
+                    // now insert d_8 and 8
+                    OnInsertedEvent(8, 2),
+                    // first 4 of the loaded items becomes placeholders: "a_4", "4", "b_5", "5"
+                    OnChangedEvent(4, 4, ITEM_TO_PLACEHOLDER),
+                    // insert e_9 and 9 using placeholders
+                    OnChangedEvent(10, 2, PLACEHOLDER_TO_ITEM),
+                    // finally, remove the last placeholder that we won't use
+                    OnRemovedEvent(12, 1)
+                ),
+                it.allEvents,
             )
         }
     }
