45be4334f981b279fcefd0ee682793d95ab74cea
Veyndan Stuart <veyndan@gmail.com>
2023-09-26 12:33:15 +0200

    Use `Scaffold` for layout in sample (#152)

--- samples/repo-search/shared-composeui/src/commonMain/kotlin/app/cash/paging/samples/reposearch/RepoSearch.kt
+++ samples/repo-search/shared-composeui/src/commonMain/kotlin/app/cash/paging/samples/reposearch/RepoSearch.kt
@@ -1,13 +1,11 @@
 package app.cash.paging.samples.reposearch
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,28 +22,33 @@ fun RepoSearchContent(
   viewModel: ViewModel,
   events: (Event) -> Unit,
 ) {
-  Surface(Modifier.fillMaxSize()) {
-    when (viewModel) {
-      ViewModel.Empty -> {
-        Column {
+  when (viewModel) {
+    ViewModel.Empty -> {
+      Scaffold(
+        topBar = {
           SearchField(
             searchTerm = "",
             events = events,
             onRefreshList = {},
           )
-        }
-      }
-      is ViewModel.SearchResults -> {
-        val repositories = viewModel.repositories.collectAsLazyPagingItems()
-        Column {
+        },
+        content = {},
+      )
+    }
+    is ViewModel.SearchResults -> {
+      val repositories = viewModel.repositories.collectAsLazyPagingItems()
+      Scaffold(
+        topBar = {
           SearchField(
             searchTerm = viewModel.searchTerm,
             events = events,
             onRefreshList = { repositories.refresh() },
           )
+        },
+        content = {
           SearchResults(repositories)
-        }
-      }
+        },
+      )
     }
   }
 }
